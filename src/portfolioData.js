fc52e3f07bd4fdbac2eb3970fcd2f2218bb149d3
Gaurav <gaurav.sinha1563@gmail.com>
2019-07-03 15:32:59 -0500

    Learning react for Work.

---
+++ src/portfolioData.js
@@ -0,0 +1,6 @@
+let portfolioData = {
+    "imagebaseurl":"https://gauravsin.github.io.git",
+    "name": "Gaurav Sinha",
+    "role": "Software Engineer on Collections Dev"
+    }
+  export default portfolioData
